0f1dec7a0fdb478173b1066945e346b4e8864b29
hdhduxhd <101924605+hdhduxhd@users.noreply.github.com>
2024-01-03 15:12:54 +0800

    Update fundus_dataloader.py

--- dataloaders/fundus_dataloader.py
+++ dataloaders/fundus_dataloader.py
@@ -92,7 +92,7 @@ class FundusSegmentation(Dataset):
 
         _img = Image.open(self.image_list[index]['image']).convert('RGB')
         _target = Image.open(self.image_list[index]['label'])
-        if _target.mode is 'RGB':
+        if _target.mode == 'RGB':
             _target = _target.convert('L')
         _img_name = self.image_list[index]['image'].split('/')[-1]
 
@@ -112,7 +112,7 @@ class FundusSegmentation(Dataset):
         for index in range(img_num):
             self.image_pool.append(Image.open(self.image_list[index]['image']).convert('RGB'))
             _target = Image.open(self.image_list[index]['label'])
-            if _target.mode is 'RGB':
+            if _target.mode == 'RGB':
                 _target = _target.convert('L')
             self.label_pool.append(_target)
             _img_name = self.image_list[index]['image'].split('/')[-1]
