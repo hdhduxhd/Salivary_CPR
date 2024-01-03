e842aed7a5f5f068fa5eabb738ea3f52b2bc7e1e
hdhduxhd <101924605+hdhduxhd@users.noreply.github.com>
2024-01-03 22:39:07 +0800

    Update custom_transforms.py

--- dataloaders/custom_transforms.py
+++ dataloaders/custom_transforms.py
@@ -496,7 +496,7 @@ class ToTensor(object):
         # torch image: C X H X W
         img = np.array(sample['image']).astype(np.float32).transpose((2, 0, 1))
         map = np.array(sample['map']).astype(np.uint8).transpose((2, 0, 1))
-        boundary = np.array(sample['boundary']).astype(np.float).transpose((2, 0, 1))
+        boundary = np.array(sample['boundary']).astype(np.float32).transpose((2, 0, 1))
         name = sample['img_name']
         img = torch.from_numpy(img).float()
         map = torch.from_numpy(map).float()
