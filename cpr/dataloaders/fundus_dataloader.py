ea7396c26e6f6407842675a79bccc17d321701d7
hdhduxhd <101924605+hdhduxhd@users.noreply.github.com>
2024-01-07 17:26:56 +0800

    Update fundus_dataloader.py

--- cpr/dataloaders/fundus_dataloader.py
+++ cpr/dataloaders/fundus_dataloader.py
@@ -238,7 +238,7 @@ class FundusSegmentation_wsim(Dataset):
 
         _img = Image.open(self.image_list[index]['image']).convert('RGB')
         _target = Image.open(self.image_list[index]['label'])
-        if _target.mode is 'RGB':
+        if _target.mode == 'RGB':
             _target = _target.convert('L')
         _img_name = self.image_list[index]['image'].split('/')[-1]
 
@@ -289,7 +289,7 @@ class FundusSegmentation_wsim(Dataset):
         for index in range(img_num):
             self.image_pool.append(Image.open(self.image_list[index]['image']).convert('RGB'))
             _target = Image.open(self.image_list[index]['label'])
-            if _target.mode is 'RGB':
+            if _target.mode == 'RGB':
                 _target = _target.convert('L')
             self.label_pool.append(_target)
             _img_name = self.image_list[index]['image'].split('/')[-3]
@@ -363,7 +363,7 @@ class FundusSegmentation_wprob(Dataset):
 
         _img = Image.open(self.image_list[index]['image']).convert('RGB')
         _target = Image.open(self.image_list[index]['label'])
-        if _target.mode is 'RGB':
+        if _target.mode == 'RGB':
             _target = _target.convert('L')
         _img_name = self.image_list[index]['image'].split('/')[-1]
 
@@ -423,7 +423,7 @@ class FundusSegmentation_wprob(Dataset):
         for index in range(img_num):
             self.image_pool.append(Image.open(self.image_list[index]['image']).convert('RGB'))
             _target = Image.open(self.image_list[index]['label'])
-            if _target.mode is 'RGB':
+            if _target.mode == 'RGB':
                 _target = _target.convert('L')
             self.label_pool.append(_target)
             _img_name = self.image_list[index]['image'].split('/')[-1]
