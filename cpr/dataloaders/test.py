41a52fbaec39b6c430a88808a8c0a1722de75d34
hdhduxhd <101924605+hdhduxhd@users.noreply.github.com>
2024-01-07 20:18:47 +0800

    Update test.py

--- cpr/dataloaders/test.py
+++ cpr/dataloaders/test.py
@@ -17,7 +17,24 @@ composed_transforms_train = transforms.Compose([
     tr.ToTensor1()
 ])
 
-db_train = DL.FundusSegmentation_wsim(base_dir='/kaggle/input/dataset', dataset='south', transform=composed_transforms_train, pseudo='/kaggle/input/checkpoint-best/pseudolabel_south.npz',radius=4)
+# 设置随机种子以确保结果可重复
+random.seed(42)
+db = DL.FundusSegmentation_wsim(base_dir='/kaggle/input/dataset', dataset='south', transform=composed_transforms_train, pseudo='/kaggle/input/checkpoint-best/pseudolabel_south.npz',radius=4)
+# 计算训练集和测试集的大小
+train_ratio = 0.7
+train_size = int(train_ratio * len(db))
+test_size = len(db) - train_size
+# 创建索引列表
+indices = list(range(len(db)))
+# 随机打乱索引
+random.shuffle(indices)
+# 根据打乱后的索引进行固定划分
+train_indices = indices[:train_size]
+test_indices = indices[train_size:]
+# 创建训练集和测试集
+db_train = torch.utils.data.dataset.Subset(db, train_indices)
+db_test = torch.utils.data.dataset.Subset(db, test_indices)
+
 train_loader = DataLoader(db_train, batch_size=8, shuffle=False, num_workers=0)
 
 def test_dataloader():
