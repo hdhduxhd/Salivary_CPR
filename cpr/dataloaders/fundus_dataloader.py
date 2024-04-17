b23bf802ebbddd1629e85f4ceacee97dcaace451
hdhduxhd <101924605+hdhduxhd@users.noreply.github.com>
2024-04-17 11:28:31 +0800

    Update fundus_dataloader.py

--- cpr/dataloaders/fundus_dataloader.py
+++ cpr/dataloaders/fundus_dataloader.py
@@ -1,7 +1,7 @@
 from __future__ import print_function, division
 import os
 import sys
-sys.path[0]='/kaggle/working/CPR/cpr'
+sys.path[0]='/kaggle/working/Salivary_CPR/cpr'
 from PIL import Image
 import numpy as np
 from torch.utils.data import Dataset
